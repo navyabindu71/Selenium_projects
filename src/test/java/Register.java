1f38500b8fb715a38ae1c850ed7ddbc41ac2f5cb
Navyabindu71 <navyabindu71@gmail.com>
2024-08-18 14:14:12 +0100

    Selenium_Project

---
+++ src/test/java/Register.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Register {
+@Test
+    public void test(){
+    WebDriver driver= new ChromeDriver();
+    driver.get("https://demo.automationtesting.in/Register.html");
+    driver.manage().window().maximize();
+    driver.findElement(By.xpath("//p[@class=\"fc-button-label\"]")).click();
+}
+}
