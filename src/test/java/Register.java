cdf1c36a9e85ddc1756c7a1f7bc6549376f14d4a
Navyabindu71 <navyabindu71@gmail.com>
2024-08-19 14:03:20 +0100

    Selenium_Project

--- src/test/java/Register.java
+++ src/test/java/Register.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Register {
@@ -10,6 +11,12 @@ public class Register {
     driver.get("https://demo.automationtesting.in/Register.html");
     driver.manage().window().maximize();
     driver.findElement(By.xpath("//p[@class=\"fc-button-label\"]")).click();
+    WebElement fname= driver.findElement(By.xpath("//body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/input[1]"));
+    fname.sendKeys("John");
+    WebElement lname= driver.findElement(By.xpath("//body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[2]/input[1]"));
+    lname.sendKeys("Katie");
+    WebElement address= driver.findElement(By.className("form-control ng-pristine ng-valid ng-touched"));
+    address.sendKeys("10 lower street, CA, CA11 022");
 
 }
 }
